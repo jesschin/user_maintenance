ecbcbade8afa1cca2b3f0afa826fe6e280d28c88
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-11-13 12:15:15 +0800

    Move user factory to spec/support folder
    RSpec is already configured to include all classes inside
    the spec/support folder so factories should be inside it.
    
    Signed-off-by: Joan Tiffany Siy <joan@teamcodeflux.com>

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -1,31 +1,14 @@
 FactoryGirl.define do
   factory :user, :class => UserMaintenance::User do
-    email 'user@example.com'
+    sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
-    role :user
+    roles_mask UserMaintenance::User.mask_for([:user].sample)
   end
 
   factory :admin, :parent => :user do
     email 'admin@example.com'
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    role :admin
-  end
-
-  factory :valid_user, :parent => :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
-  end
-
-  factory :valid_admin, :parent => :admin do
-    sequence(:email) { |n| "admin#{n}@example.com" }
-  end
-
-  factory :invalid_user, :class => UserMaintenance::User do
-    email nil
-    first_name nil
-    last_name nil
-    password '123'
+    roles_mask UserMaintenance::User.mask_for(:admin)
   end
 end
