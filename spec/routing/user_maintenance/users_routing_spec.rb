b84f22af2447afcd87294a1744ddfb702d543648
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-11-13 12:11:46 +0800

    Update routing spec
    Included additional routes for profile and password
    reset. Update routing tests and used url helpers.
    
    Signed-off-by: Joan Tiffany Siy <joan@teamcodeflux.com>

--- spec/routing/user_maintenance/users_routing_spec.rb
+++ spec/routing/user_maintenance/users_routing_spec.rb
@@ -3,34 +3,77 @@ require "rails_helper"
 module UserMaintenance
   RSpec.describe UsersController, :type => :routing do
     describe "routing" do
+      routes { UserMaintenance::Engine.routes }
 
-      # it "routes to #index" do
-      #   expect(:get => "/user_maintenance/users").to route_to("users#index")
-      # end
+      it "routes to #index" do
+        expect(:get => users_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'index')
+      end
 
-      # it "routes to #new" do
-      #   expect(:get => "/users/new").to route_to("users#new")
-      # end
+      it "routes to #new" do
+        expect(:get => new_user_path).to route_to(:controller => 'user_maintenance/users',
+                                                  :action => 'new')
+      end
 
-      # it "routes to #show" do
-      #   expect(:get => "/users/1").to route_to("users#show", :id => "1")
-      # end
+      it "routes to #show" do
+        expect(:get => user_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'show', :id => '1')
+      end
 
-      # it "routes to #edit" do
-      #   expect(:get => "/users/1/edit").to route_to("users#edit", :id => "1")
-      # end
+      it "routes to #edit" do
+        expect(:get => edit_user_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                                    :action => 'edit', :id => '1')
+      end
 
-      # it "routes to #create" do
-      #   expect(:post => "/users").to route_to("users#create")
-      # end
+      it "routes to #create" do
+        expect(:post => users_path).to route_to(:controller => 'user_maintenance/users',
+                                              :action => 'create')
+      end
 
-      # it "routes to #update" do
-      #   expect(:put => "/users/1").to route_to("users#update", :id => "1")
-      # end
+      it "routes to #update" do
+        expect(:patch => user_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'update', :id => '1')
+      end
 
-      # it "routes to #destroy" do
-      #   expect(:delete => "/users/1").to route_to("users#destroy", :id => "1")
-      # end
+      it "routes to #edit_password" do
+        expect(:get => edit_password_profile_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'edit_password')
+      end
+
+      it "routes to #update_password" do
+        expect(:put => update_password_profile_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'update_password')
+      end
+
+      it "routes to profile #show" do
+        expect(:get => profile_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'show')
+      end
+
+      it "routes to profile #edit" do
+        expect(:get => edit_profile_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'edit')
+      end
+
+      it "routes to profile #update" do
+        expect(:put => profile_path).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'update')
+      end
+
+      it "routes to #resetting_password" do
+        expect(:get => user_reset_password_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'resetting_password', :user_id => '1')
+      end
+
+      it "routes to #resetting_password" do
+        expect(:put => user_reset_password_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'reset_password', :user_id => '1')
+      end
+
+      it "routes to #enable" do
+        expect(:put => user_enable_path(1)).to route_to(:controller => 'user_maintenance/users',
+                                               :action => 'enable', :user_id => '1')
+      end
 
     end
   end
