fb02653b8db307db95b5df7fe7c832b282cbed74
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-11-13 12:15:34 +0800

    Move User Spec to the correct folder
    Signed-off-by: Joan Tiffany Siy <joan@teamcodeflux.com>

--- spec/models/user_maintenance/user_spec.rb
+++ spec/models/user_maintenance/user_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 module UserMaintenance
   RSpec.describe User, :type => :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'has a valid factory' do
+      expect(create(:user)).to be_valid
+    end
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:roles_mask) }
+
+    it "returns the user's full name as a string" do
+      user = create(:user, :first_name => 'John', :last_name => 'Doe')
+      expect(user.full_name).to be_eql 'John Doe'
+    end
   end
 end
