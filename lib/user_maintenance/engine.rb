41b1fb644ba7fcb8fd351f04d916674b7ad3876d
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-11-18 11:21:01 +0800

    Correct wrong directory for factories
    Signed-off-by: Joan Tiffany Siy <joan@teamcodeflux.com>

--- lib/user_maintenance/engine.rb
+++ lib/user_maintenance/engine.rb
@@ -6,7 +6,7 @@ module UserMaintenance
       g.template_engine :haml
 
       g.test_framework :rspec
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.fixture_replacement :factory_girl, :dir => 'spec/support/factories'
     end
   end
 end
