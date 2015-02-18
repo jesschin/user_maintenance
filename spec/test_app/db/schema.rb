1c92399626513d8b92432a8a1bc91181e01c0036
Jess Malvin Chin <jess@teamcodeflux.com>
2015-02-18 12:35:42 +0800

    Modify the tests to use a more specific query of finding the test comparison
    Signed-off-by: Jess Malvin Chin <jess@teamcodeflux.com>

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20141111050937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user_maintenance_users", force: true do |t|
+  create_table "user_maintenance_users", force: :cascade do |t|
     t.string   "email",                            default: "",   null: false
     t.string   "encrypted_password",               default: "",   null: false
     t.string   "reset_password_token"
